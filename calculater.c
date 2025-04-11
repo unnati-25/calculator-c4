02b9e2b0ce9ab961a632de04f6a5850a9cf6ba39
Vishwanath-Akshay <mail2akshay14@gmail.com>
2025-04-11 11:52:54 +0530

    team calculator in c

---
+++ calculater.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include <math.h>
+
+ int add(int a, int b){
+     return a+b;
+ }
+ int sub(){
+
+ }
+ int mult(){
+
+ }
+
+ int div(){
+
+ }
+ int power(){
+
+ }
+
+
+int main(){
+
+    int a,b;
+    printf("Welcome to the Team Calculator!\n");
+    printf("Enter first number: ");
+    scanf("%d", &a);
+    printf("Enter second number: ");
+    scanf("%d", &b);
+
+
+
+    printf("Addition of two numbers is:" ,add(a,b));
+     
+}
